99b5f5bfc0c03f2be67bbee157de799fa1b432d9
jugendhacker <julian.ribbeck@gmx.de>
2015-06-06 16:53:53 +0200

    Arduino sketch

--- sketch_jun06a/sketch_jun06a/sketch_jun06a.ino
+++ sketch_jun06a/sketch_jun06a/sketch_jun06a.ino
@@ -10,13 +10,16 @@ void setup() {
 void loop(){
   int Distance = analogRead(A0);
   delay(1000);
-  Serial.println(Distance);
-  if(Distance >= 500){
+  if(Distance <= 654.72){
     digitalWrite(12, HIGH);
     digitalWrite(13, LOW);
+    String fuelstand = "voll!";
+    Serial.println(fuelstand);
   }
   else{
     digitalWrite(12, LOW);
     digitalWrite(13, HIGH);
+    String fuelstand = "noch nicht voll";
+    Serial.println(fuelstand);
     }
 }
